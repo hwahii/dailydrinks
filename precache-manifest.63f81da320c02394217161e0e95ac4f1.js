42e6906f9b84c76f561d8bf5f1d78f6db83e6b84
Huang-Yin Chung <in1102@livemail.tw>
2019-11-22 15:34:22 +0800

    Updates

---
+++ precache-manifest.63f81da320c02394217161e0e95ac4f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675a8bad7c372a495506da5af4333be3",
+    "url": "/dailydrinks/index.html"
+  },
+  {
+    "revision": "34ea97080c6e88e2ecef",
+    "url": "/dailydrinks/static/css/main.11540f44.chunk.css"
+  },
+  {
+    "revision": "c9a5fdb24519ff5460a8",
+    "url": "/dailydrinks/static/js/2.20a22520.chunk.js"
+  },
+  {
+    "revision": "34ea97080c6e88e2ecef",
+    "url": "/dailydrinks/static/js/main.b7c11767.chunk.js"
+  },
+  {
+    "revision": "a9742a9eef63316be08c",
+    "url": "/dailydrinks/static/js/runtime-main.ca5f9e86.js"
+  },
+  {
+    "revision": "c63c2575a2784527a6dd1bb39bb9a987",
+    "url": "/dailydrinks/static/media/logo.c63c2575.png"
+  },
+  {
+    "revision": "78e089b7963c7636904566e866fe197c",
+    "url": "/dailydrinks/static/media/menu.78e089b7.png"
+  }
+]);
